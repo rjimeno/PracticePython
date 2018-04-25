93da9d5aa53e4e6dc06370f95781392d3633ff30
Roberto Jimeno <roberto.jimeno@thomsonreuters.com>
2018-04-25 12:59:11 -0400

    Deleted commented-out code.

--- e17.py
+++ e17.py
@@ -16,9 +16,3 @@ story_headings = soup.find_all(class_="story-heading")
 
 for t in story_headings:
     print(t.a.string)
-
-#for story_heading in soup.find_all(class_="story-heading"): 
-#    if story_heading.a: 
-#        print(story_heading.a.text.replace("\n", " ").strip())
-#    else: 
-#        print(story_heading.contents[0].strip())
