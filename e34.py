5e3d2cc447d9fd87d2be88581f8b001efa656398
Roberto Jimeno <roberto.jimeno@gmail.com>
2018-06-05 20:05:40 -0400

    mend

--- e34.py
+++ e34.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 """
 This program used as its original source of data the file available here:
 https://raw.githubusercontent.com/alebelcor/celeb-birthdays/master/output/celeb-birthdays.json
@@ -7,11 +6,10 @@ https://raw.githubusercontent.com/alebelcor/celeb-birthdays/master/output/celeb-
 BIG THANKS for that file. I the author believes my copying the file is wrong,
 I'll delete the file from muy repository.
 """
-
 import json
 
 
-def load(file):
+def load(file_name):
     '''
     Takes a JSON file (name) as input.
     Output is a data structure that represents the content of the file.
@@ -19,17 +17,15 @@ def load(file):
     the data structure that will be returned.
     '''
     data = {}
-    with open(file, "r") as f:
-        date = json.load(f)
-
-    for d in date:
-        for name in date[d]:
-            data[name]=d
-        
+    with open(file_name, "r") as file:
+        date = json.load(file)
+    for m_d in date:
+        for name in date[m_d]:
+            data[name] = m_d
     return data
 
 
-def dump(data, file):
+def dump(data, file_name):
     '''
     Takes a data structure and a file name as input.
     Output: True if the data dump was successful. False otherwise.
@@ -38,12 +34,12 @@ def dump(data, file):
     Side effect: a file is created.
     '''
     try:
-        with open(file, "w") as f:
-            json.dump(data, f)
-    except:
-        print("There was a problem dumping data to a file.")
-        return False
-    return True
+        with open(file_name, "w") as file:
+            json.dump(data, file)
+        return True
+    except (OSError, IOError) as exception:
+        print("There was a problem dumping data to a file: {}.".format(exception))
+    return False
 
 
 
@@ -71,10 +67,9 @@ def better_birthday_dictionary(b_d):
     print(name + "'s birthday is " + b_d[name])
 
 if __name__ == '__main__':
-    data = {}
     print('Program is being run by itself.')
-    data = load("celeb-birthdays.json")
-    dump(data, "celeb-birthdays-output.json")
-    better_birthday_dictionary(data)
+    BIRTHDAY_OF = load("celeb-birthdays.json")
+    dump(BIRTHDAY_OF, "celeb-birthdays-output.json")
+    better_birthday_dictionary(BIRTHDAY_OF)
 else:
     print('Functions are being imported from another module.')
