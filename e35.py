617de0103f1b5497f2e144ae18e843b31b6ce0f5
Roberto Jimeno <roberto.jimeno@gmail.com>
2018-08-12 10:20:59 -0400

    Works even though is clearly not elegant.

--- e35.py
+++ e35.py
@@ -7,61 +7,48 @@ BIG THANKS for that file. I the author believes my copying the file is wrong,
 I'll delete the file from muy repository.
 """
 import json
+#from collections import Counter
 
-
-def load(file_name):
+def load_return_histogram(file_name):
     '''
     Takes a JSON file (name) as input.
     Output is a data structure that represents the content of the file.
     Side effects: Read the content of the file from storage and populates
     the data structure that will be returned.
     '''
-    data = {}
+    monthly_histogram = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     with open(file_name, "r") as file:
         date = json.load(file)
     for m_d in date:
-        month, day = m_d.split('-')
-        for name in date[m_d]:
-            data[name] = { 'month': month, 'day': day }
-    return data
+        tmp = m_d.split('-')
+        month = int(tmp[0])-1
+        monthly_histogram[month] += len(date[m_d])
+    return monthly_histogram
 
 
-def dump(data, file_name):
-    '''
-    Takes a data structure and a file name as input.
-    Output: True if the data dump was successful. False otherwise.
-    Side effect: creates a file (with the file name) that contains a
-    representation of the data structure.
-    Side effect: a file is created.
-    '''
-    try:
-        with open(file_name, "w") as file:
-            json.dump(data, file)
-        return True
-    except (OSError, IOError) as exception:
-        print("There was a problem dumping data to a file: {}.".format(exception))
-    return False
+def dump_count(data):
+    'Produces JSON output.'
 
-
-
-def simple_histogram(b_d):
-    count_for_month = dict()
-    for date in b_d.values():
-        month = int(date['month'])
-        if month in count_for_month.keys():
-            count_for_month[month] += 1
-        else:
-            count_for_month[month] = 1
-    print(count_for_month)
-    print("{\n")
-    for month in count_for_month.keys():
-        print("    \"{}\": \"{}\"\n".format(month, count_for_month[month]))
-    print("}\n")
+    print("{")
+    for month in MONTH:
+        print("    '{}': '{}',".format(MONTH[month], data[month]))
+    print("}")
 
 if __name__ == '__main__':
-    print('Program is being run by itself.')
-    BIRTHDAY_OF = load("celeb-birthdays.json")
-    dump(BIRTHDAY_OF, "celeb-birthdays-output.json")
-    simple_histogram(BIRTHDAY_OF)
+    MONTH = {}
+    MONTH[0] = 'January'
+    MONTH[1] = 'February'
+    MONTH[2] = 'March'
+    MONTH[3] = 'April'
+    MONTH[4] = 'May'
+    MONTH[5] = 'June'
+    MONTH[6] = 'July'
+    MONTH[7] = 'August'
+    MONTH[8] = 'September'
+    MONTH[9] = 'October'
+    MONTH[10] = 'November'
+    MONTH[11] = 'December'
+    BIRTHDAY_OF = load_return_histogram("celeb-birthdays.json")
+    dump_count(BIRTHDAY_OF)
 else:
     print('Functions are being imported from another module.')
