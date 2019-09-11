0a73e6982b87078089725ee2417f1ab6fd098238
Roberto Jimeno <roberto.jimeno@gmail.com>
2019-09-11 12:17:29 -0400

    Extra credits in the (e3.py) List Less Than Ten exercise.

--- e3.py
+++ e3.py
@@ -1,8 +1,57 @@
 #!/usr/bin/env python3
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-
+default_limit = 5
 for x in a:
-    if x < 5:
+    if x < default_limit:
         print(x)
 
+# Extras:
+
+# 1.Instead of printing the elements one by one, make a new list that has all
+# the elements less than 5 from this list in it and print out this new list.
+def list_less_than( input_array, limit=default_limit):
+    """
+    >>> list_less_than([])
+    []
+    >>> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    >>> list_less_than(a)
+    [1, 1, 2, 3]
+    >>> list_less_than(a, 8)
+    [1, 1, 2, 3, 5]
+    """
+    l = []
+    for n in input_array:
+        if n < limit:
+            l.append(n)
+    return l
+
+# 2. Write this in one line of Python.
+def list_less_than_oneliner(i_a, l=default_limit):
+    """
+    >>> list_less_than([])
+    []
+    >>> a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    >>> list_less_than_oneliner(a)
+    [1, 1, 2, 3]
+    >>> list_less_than_oneliner(a, 8)
+    [1, 1, 2, 3, 5]
+    """
+    return [n for n in i_a if n < l]
+
+try:
+    limit = int(input("What's the smallest number you don't care about (i.e. "
+                      "What's the limit? Default is {}.)" .format(
+        default_limit)))
+except Exception as e:
+    print("That did not look like a number: '{}'".format(e))
+    print("Will default of {} instead.".format(default_limit),
+          file=sys.stderr)
+    limit = default_limit
+print(list_less_than_oneliner(a, limit))
+
+
+
+if '__main__' == __name__:
+    import doctest
+    doctest.testmod()
