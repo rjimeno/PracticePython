3ff7835e7d6f458327ce8402fb04c441dfd73a2c
Roberto Jimeno <roberto.jimeno@gmail.com>
2019-09-11 12:53:25 -0400

    Added one Extra feature to e5.py

--- e5.py
+++ e5.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-
+import random
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
@@ -11,3 +11,30 @@ for x in a:
         c[x]=True
 l = list(c.keys())
 print(l)
+
+# Extras:
+# 1. Randomly generate two lists to test this.
+
+def generate_randomized_list():
+    THIS_MAX_INTEGER = 20
+    THIS_MAX_LENGTH = 19
+    new_list = []
+    for i in range(0, THIS_MAX_LENGTH):
+        new_list.append(random.randrange(THIS_MAX_INTEGER))
+    return new_list
+
+c = generate_randomized_list()
+d = generate_randomized_list()
+
+def intersection(left, right):
+    both = set()
+    for x in left:
+        if x in set(right):
+            both.add(x)
+    return both
+print("c == {}".format(c))
+print("d == {}".format(d))
+inter = intersection(c, d)
+print("The intersection of both sets  above is: {}".format(inter))
+
+
