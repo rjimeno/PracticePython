910e4cddebc85a628d163c8052406195e7006e49
rjimeno <roberto.jimeno@gmail.com>
2024-03-09 17:36:01 -0500

    Followed Pylint advice.

--- e9.py
+++ e9.py
@@ -2,24 +2,24 @@
 
 import random
 
-min = 1
-max = 9
+MIN = 1
+MAX = 9
 
-print("I'll select a integer between {} and {} randomly.".format(min, max))
+print(f"I'll select a integer between {MIN} and {MAX} randomly.")
 
-s = random.randint(min, max)
+s = random.randint(MIN, MAX)
 
 while True:
     g = int(input("Guess which one I picked?: ").strip())
     print("")
-    if g < min or max < g:
+    if g < MIN or MAX < g:
         continue
     if g < s:
         print("Too LOW, try again.")
         continue
-    elif s < g:
+    if s < g:
         print("Too HIGH, try again.")
         continue
     break
 
-print("You got it! It was number {}.".format(s))
+print(f"You got it! It was number {s}.")
