359ed7b87cfdbcc5d9df86e3e87eacfd544cdbfe
rjimeno <roberto.jimeno@gmail.com>
2024-03-09 12:28:17 -0500

    Followed Pylint's advice.

--- e3.py
+++ e3.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
 # Changelog:
+# + 20230309:
+# - Followed Pylint advice on names of constants and variables and f-strings.
+# - Stopped using numbers.Number for type hints.
 # + 20230902:
 # - Added type hints.
 # - import sys to prevent the IDE (or linter) from warning.
 
-from numbers import Number
 import sys
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-default_limit = 5
+DEFAULT_LIMIT = 5
 for x in a:
-    if x < default_limit:
+    if x < DEFAULT_LIMIT:
         print(x)
 
 # Extras:
 
 # 1.Instead of printing the elements one by one, make a new list that has all
 # the elements less than 5 from this list in it and print out this new list.
-def list_less_than(input_array: list, limit: Number=default_limit) -> list:
+def list_less_than(input_array: list, limit_int: int=DEFAULT_LIMIT) -> list:
     """
     >>> list_less_than([])
     []
@@ -30,12 +32,12 @@ def list_less_than(input_array: list, limit: Number=default_limit) -> list:
     """
     l = []
     for n in input_array:
-        if n < limit:
+        if n < limit_int:
             l.append(n)
     return l
 
 # 2. Write this in one line of Python.
-def list_less_than_oneliner(i_a: list, l: Number=default_limit) -> list:
+def list_less_than_oneliner(i_a: list, l: int=DEFAULT_LIMIT) -> list:
     """
     >>> list_less_than([])
     []
@@ -48,14 +50,15 @@ def list_less_than_oneliner(i_a: list, l: Number=default_limit) -> list:
     return [n for n in i_a if n < l]
 
 try:
-    limit = int(input("What's the smallest number you don't care about (i.e. "
-                      "What's the limit? Default is {}.)" .format(
-        default_limit)))
+    limit = int(input(
+        "What's the smallest number you don't care about (i.e. "
+        f"What's the limit? Default is {DEFAULT_LIMIT}."
+    ))
 except Exception as e:
-    print("That did not look like a number: '{}'".format(e))
-    print("Will default of {} instead.".format(default_limit),
+    print(f"That did not look like a number: '{e}'")
+    print(f"Will default of {DEFAULT_LIMIT} instead.",
           file=sys.stderr)
-    limit = default_limit
+    limit = DEFAULT_LIMIT
 print(list_less_than_oneliner(a, limit))
 
 
