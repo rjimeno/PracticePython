ece76040a59c1d3e574fd7b6763df8392fbc5a44
rjimeno <roberto.jimeno@gmail.com>
2024-03-09 19:09:39 -0500

    Pylint, cleaning and unit tests.

--- e11.py
+++ e11.py
@@ -1,40 +1,56 @@
 #!/usr/bin/env python3
 
 # CHANGELOG:
+# + 20240309:
+# -
 # + 20230902:
 # - More clearly separated __main__ and increase
 #   use of functions (not elegantly, though).
 
-def yes(number):
-    print("{} is a prime number!".format(number))
-    exit(0)
+import sys
 
-def no(number):
-    print("{} is NOT a prime number".format(number))
-    exit(0)
+def yes(n: int):
+    """
+    >>> yes(3)
+    3 is a prime number!
+    """
+    print(f"{n} is a prime number!")
 
-def is_prime_number(number: int) -> None:
-    if number <= 0:
+def no(n: int):
+    """
+    >>> no(4)
+    4 is NOT a prime number
+    """
+    print(f"{n} is NOT a prime number")
+
+def is_prime_number(n: int) -> None:
+    """
+    >>> is_prime_number(5039)
+    5039 is a prime number!
+    >>> # is_prime_number(39916801) # May take up to two seconds.
+    39916801 is a prime number!
+    >>> # is_prime_number(479001599) # May take up to ten seconds.
+    479001599 is a prime number!
+    """
+    if n <= 0:
         print("That's not a positive integer!")
-        exit(1)
-    if 0 < number < 4:
-        yes(number)
-    if 4 == number: 
-        no(number)
-    for n in range(2, int(number/2)):
-        if 0 == number%n:
-            no(number)
-        else:
-            continue
-    yes(number)
+        sys.exit(1)
+    if 0 < n < 4:
+        yes(n)
+        return
+    for i in range(2, int(n/2)):
+        if 0 == n%i:
+            no(n)
+            return
+    yes(n)
 
 if "__main__" == __name__:
-    number = input("Provide me with a positive integer: ").strip()
+    import doctest
+    doctest.testmod()
+
+    number_s = input("Provide me with a positive integer: ").strip()
     try:
-        number = int(number)
+        number_i = int(number_s)
     except ValueError:
-        exit(1)
-    is_prime_number(number)
-
-
-
+        sys.exit(1)
+    is_prime_number(number_i)
