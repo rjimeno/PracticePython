2de1cc2baa38f0cf47bd087a3ecc0ca8d064f86e
rjimeno <roberto.jimeno@gmail.com>
2024-03-10 12:49:39 -0400

    Small improvements.

--- e15.py
+++ e15.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
-long_string = "My name is Michele"
+import sys
 
 def rwo(ls):
+    """
+    >>> rwo("My name is Michele")
+    ['Michele', 'is', 'name', 'My']
+    """
     words = ls.split()
     words.reverse()
-        
     return words
 
-print(rwo(long_string))
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
+    try:
+        s = input("Enter the input to be word-reversed: ")
+    except Exception as e:
+        sys.exit(-1)
+    print(" ".join(rwo(s)))
