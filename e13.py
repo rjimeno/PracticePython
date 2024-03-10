a792e3d36484cbe5b42e7b1879a396c745845589
rjimeno <roberto.jimeno@gmail.com>
2024-03-10 12:01:09 -0400

    Cleaned, organized and unit-tested.

--- e13.py
+++ e13.py
@@ -1,19 +1,52 @@
 #!/usr/bin/env python3
 
-n = int(input("How many Fibonacci numbers?: "))
+# Changelog:
+# + 20240310:
+# - Error and exception handling.
+# - Reorganized as functions with unit tests.
 
-def f(n):
+import sys
+
+def nth_fib(n):
+    """
+    >>> nth_fib(1)
+    0
+    >>> nth_fib(2)
+    1
+    >>> nth_fib(3)
+    1
+    >>> nth_fib(4)
+    2
+    """
     if n < 1:
-        exit(1)
+        sys.exit(n)
     elif n == 1:
-        return 1
+        return 0
     elif n == 2:
         return 1
     else:
-        return f(n-2)+f(n-1)
+        return nth_fib(n - 2) + nth_fib(n - 1)
+    
+def fib_sequ_of_n(n):
+    """
+    >>> fib_sequ_of_n(0)
+    []
+    >>> fib_sequ_of_n(3)
+    [0, 1, 1]
+    >>> fib_sequ_of_n(7)
+    [0, 1, 1, 2, 3, 5, 8]
+    """
+    result=[]
+    for i in range(1, n + 1):
+        result.append(nth_fib(i))
+    print(result)
 
-result=[]
-for i in range(1, n+1):
-    result.append(f(i))
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
 
-print(result)
+    try:
+        length = int(input("How many Fibonacci numbers?: "))
+    except ValueError:
+        sys.exit(-1)  # $? is 255
+    fib_sequ_of_n(length)
