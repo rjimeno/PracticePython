405c7f6213c4932dd474b93d888d3d21dda8478b
rjimeno <roberto.jimeno@gmail.com>
2024-03-10 14:35:34 -0400

    Improved error handling.

--- e17.py
+++ e17.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
+# Changelog:
+# + 20230310:
+# - Added timeout to the request.
+
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 OK=200
 URL='https://www.nyt.com'
 
-r = requests.get(URL)
+r = requests.get(URL, timeout=(7, 10))  # https://requests.readthedocs.io/en/latest/user/advanced/#timeouts
 if r.status_code != OK:
     print("There was a problem with the request.")
-    exit(1)
+    sys.exit(1)
 
 soup = BeautifulSoup(r.text, "html.parser")
 
