56bbc82298f19f46c3c26194b5bc383334140e50
rjimeno <roberto.jimeno@gmail.com>
2024-03-10 16:19:25 -0400

    Updated to familizarize with Beautiful Soup.

--- e19.py
+++ e19.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 
+# Changelog:
+# + 20230310:
+# - Modified (again) because the source changed and didn't work anymore.
+@ - https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
 import requests
+import sys
 from bs4 import BeautifulSoup
 
 OK=200
+URL='https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture'
 
-r = requests.get('https://www.vanityfair.com/style/society/2014/06/monica-lewinsky-humiliation-culture')
+
+r = requests.get(URL, timeout=(7, 10))
 if r.status_code != OK:
     print("There was a problem with the request.")
-    exit(1)
+    sys.exit(1)
 
 soup = BeautifulSoup(r.text, "html.parser")
 
-content_sections = soup.find_all(class_="main-content")  # Used to be "content-section"
+content_sections = soup.find_all('a')  # Used to be "class_=content-section"
 
 for t in content_sections:
-    #print(t.p)
     print()
+    print(t.get('href'))  # Used to be print(t.p)
+print()
