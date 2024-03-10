d10168206f2b02da6a5672985e01dcda26c3ef54
rjimeno <roberto.jimeno@gmail.com>
2024-03-09 19:19:40 -0500

    Unit test and cleanup.

--- e12.py
+++ e12.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-a = [5, 10, 15, 20, 25]
-
 def ht(l):
-    head = l[0]
-    tail = l[-1]
-    return [head, tail]
+    """
+    >>> ht([5, 10, 15, 20, 25])
+    [5, 25]
+    """
+    return [l[0], l[-1]]
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
