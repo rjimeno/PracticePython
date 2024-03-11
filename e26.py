735acda1c45376a050d56a45fce29e3f4179cb8a
rjimeno <roberto.jimeno@gmail.com>
2024-03-11 12:39:09 -0400

    Minor improvements.

--- e26.py
+++ e26.py
@@ -1,54 +1,56 @@
 #!/usr/bin/env python3
 
-def fourOfAkind(a, B):
-    if a == B[0]:
-        if a == B[1]:
-            if a == B[2]:
-                return True
+def all_the_same(pid: int, line: list[int]) -> bool:
+    """ Returns True if all the elements in list B have value a. """
+    return all(pid == line[i] for i in [0, 1, 2])
 
-def playerWinsOnBoard(a, b):
-    if (fourOfAkind(a,b[0]) or
-        fourOfAkind(a,b[1]) or
-        fourOfAkind(a,b[2]) or
-        fourOfAkind(a,[b[0][0], b[1][0], b[2][0]]) or
-        fourOfAkind(a,[b[0][1], b[1][1], b[2][1]]) or
-        fourOfAkind(a,[b[0][2], b[1][2], b[2][2]]) or
-        fourOfAkind(a,[b[0][0], b[1][1], b[2][2]]) or
-        fourOfAkind(a,[b[0][2], b[1][1], b[2][0]])
+def player_wins_on_board(pid: int, b: list[list[int]]) -> bool:
+    """ Returns true if the board if the board is in a winning configuration
+    for the player with 'pid' Returns false if no winner can be chosen (yet)."""
+    if (any(all_the_same(pid,b[i]) for i in [0, 1, 2]) or
+        any(all_the_same(pid,[b[0][i], b[1][i], b[2][i]]) for i in [0, 1, 2]) or
+        all_the_same(pid,[b[0][0], b[1][1], b[2][2]]) or
+        all_the_same(pid,[b[0][2], b[1][1], b[2][0]])
         ):
         return True
     return False
 
-def winnerOfBoardIs(board):
-    # Board is a 3-list of 3-lists where items can be either the integers 0, 1 or 2.
-    if playerWinsOnBoard(2, board):
+def winner_of_board_is(board: list[list[int]]):
+    """ Board is a 3-list of 3-lists where items can be
+    either the integers 0, 1 or 2."""
+    if player_wins_on_board(2, board):
         print("Player two wins!")
-    elif playerWinsOnBoard(1, board):
+    elif player_wins_on_board(1, board):
         print("Player one wins!")
     else:
         print("No player wins!")
 
-winner_is_2 = [[2, 2, 0],
+winner_is_2 = [
+    [2, 2, 0],
 	[2, 1, 0],
 	[2, 1, 1]]
-winnerOfBoardIs(winner_is_2)
+winner_of_board_is(winner_is_2)
 
-winner_is_1 = [[1, 2, 0],
+winner_is_1 = [
+    [1, 2, 0],
 	[2, 1, 0],
 	[2, 1, 1]]
-winnerOfBoardIs(winner_is_1)
+winner_of_board_is(winner_is_1)
 
-winner_is_also_1 = [[0, 1, 0],
+winner_is_also_1 = [
+    [0, 1, 0],
 	[2, 1, 0],
 	[2, 1, 1]]
-winnerOfBoardIs(winner_is_also_1)
+winner_of_board_is(winner_is_also_1)
 
-no_winner = [[1, 2, 0],
+no_winner = [
+    [1, 2, 0],
 	[2, 1, 0],
 	[2, 1, 2]]
-winnerOfBoardIs(no_winner)
+winner_of_board_is(no_winner)
 
-also_no_winner = [[1, 2, 0],
+also_no_winner = [
+    [1, 2, 0],
 	[2, 1, 0],
 	[2, 1, 0]]
-winnerOfBoardIs(also_no_winner)
+winner_of_board_is(also_no_winner)
