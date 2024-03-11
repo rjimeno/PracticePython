fb60ec51b45a5486828039af7959a341008f5c2a
rjimeno <roberto.jimeno@gmail.com>
2024-03-11 13:15:44 -0400

    Followed some Pylint advice.

--- e27.py
+++ e27.py
@@ -3,44 +3,47 @@
 def display(board):
     print("")
     print("-------------")
-    for r in range(0, len(board)):
-        print("| " + board[r][0] + " | " + board[r][1] + " | " + board[r][2] + " |")
+    for b in board:
+        print("| " + b[0] + " | " + b[1] + " | " + b[2] + " |")
         print("-------------")
     print("")
 
-D = " XO" # D is an array of char (that should be constant).
- 
-PLAYERS=(1, 2)
+def main():
+    game = [[" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "]]
 
-game = [[" ", " ", " "],
-	[" ", " ", " "],
-	[" ", " ", " "]]
-
-available_moves = 9 
-player = 1
-while 0 < available_moves:
-    try:
-        i = input("Where do you want to put your {} Player {}?\nValues for either can be 1, 2 or 3 only. <row>, <column>: ".format(D[player], player))
-        row = int(i[0].strip()) - 1
-        column = int(i[-1].strip()) - 1
-        assert(0 <= row <= 2 and 0 <= column <= 2) # Would trigger "index out of range" exception.
-    except:
-        print("")
-        print("There was a problem with the values you gave me.")
-        print("")
-        continue
-    if D[1] != game[row][column] and D[2] != game[row][column]:
-        game[row][column] = D[player]
-        available_moves -= 1
-    else:
-        print("")
-        print("Row {}, column {} has already been used. you will have to select a different one!".format(row + 1, column + 1))
-        print("")
+    available_moves = 9 
+    player = 1
+    while 0 < available_moves:
+        try:
+            i = input(f"Where do you want to put your {D[player]} Player {player}?"
+                    "\nValues for either can be 1, 2 or 3 only. <row>, <column>: ")
+            row = int(i[0].strip()) - 1
+            column = int(i[-1].strip()) - 1
+            assert 0 <= row <= 2 and 0 <= column <= 2 # Would trigger "index out of range" exception.
+        except (ValueError, IndexError, AssertionError):
+            print("")
+            print("Please check your input. There was a problem with the values you gave me.")
+            print("")
+            continue
+        if D[1] != game[row][column] and D[2] != game[row][column]:
+            game[row][column] = D[player]
+            available_moves -= 1
+        else:
+            print("")
+            print(f"Row {row + 1}, column {column + 1} has already been used. you will have to select a different one!")
+            print("")
+            display(game)
+            print("")
+            continue
         display(game)
-        print("")
-        continue
-    display(game)
-    if 1 == player:
-        player = 2
-    else:
-        player = 1
+        if 1 == player:
+            player = 2
+        else:
+            player = 1
+
+D = " XO" # D is an array of char (that should be constant).
+PLAYERS=(1, 2)
+if __name__ == "__main__":
+    main()
