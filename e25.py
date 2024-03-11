10359fa85095bffbcaa9461b7c740ae6ef6d214c
rjimeno <roberto.jimeno@gmail.com>
2024-03-11 12:05:01 -0400

    Improved interaction and reorganized code to reduce Pylint alerts.

--- e25.py
+++ e25.py
@@ -1,25 +1,29 @@
 #!/usr/bin/env python3
 
-print("Think of an integer in the interval [0..100] and write it down.")
-input("Press the Enter key once you are done.")
-correct = 'no'
-guess = 50
-delta = 50
-while correct != 'yes':
-    delta = delta // 2
-    if delta <= 1:
-        delta = 1
-    s = "Is it {} ? Reply with either 'yes', 'too high' or 'too low': ".format(guess)
-    correct = input(s)
-    r = str(correct)
-    if r == 'too high':
-        guess -= delta
-    elif r == 'too low':
-        guess += delta
-    elif r == 'yes':
-        print("Great!")
-    else:
-        print("Really?")
-    
-            
-      
+def guessing_game_two():
+    """ The computer has to guess a number."""
+    print("Think of an integer in the interval [0..100] and write it down.")
+    input("Press the Enter key once you are done. ")
+    correct = 'no'
+    guess = 50
+    delta = 50
+    while correct != 'yes':
+        if guess < 0 or 100 <= guess:
+            print("Did you make a mistake above? I detect an error.")
+            return
+        delta = delta // 2
+        delta = max(1, delta)
+        s = f"Is it {guess} ? Reply with either 'yes', 'too high' or 'too low': "
+        correct = input(s)
+        r = str(correct)
+        if r == 'too high':
+            guess -= delta
+        elif r == 'too low':
+            guess += delta
+        elif r == 'yes':
+            print("Great!")
+        else:
+            print("What?")  
+
+if __name__ == '__main__':
+    guessing_game_two()
